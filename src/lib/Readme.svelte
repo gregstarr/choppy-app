421c069481657f543581046b18dde436fb2febc8
Greg Starr <gregstarr2@gmail.com>
2023-02-01 17:06:43 +0000

    feat(readme): adding site description and images

--- src/lib/Readme.svelte
+++ src/lib/Readme.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
 </script>
 
-<h1>Readme</h1>
+<h1>Choppy</h1>
+
+<p>
+  A free online tool to split up large 3D models (.stl or .3mf) into parts that can fit
+  on your printer.
+</p>
+
+<div class="image-container">
+  <img class="image-item" src="/process3.png" alt="">
+  <img class="image-item" src="/shoerack2.png" alt="">
+</div>
+
 
 <h3>Instructions</h3>
 <ol>
@@ -31,71 +42,23 @@
 <h3>Details</h3>
 <p>
   This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
-  This is where the readme will go. This is where the readme will go. This is where the readme will go. 
+ 
 </p>
 
 
 
 <style>
-  h3 {
+  h1, h3 {
     margin-bottom: 15px;
+  }
+  .image-container {
+    display: flex;
+    column-gap: 20px;
+    justify-content: space-around;
+  }
+  .image-item {
+    max-height: 400px;
+    border: 8px solid lightslategrey;
+    border-radius: 20px;
   }
 </style>
